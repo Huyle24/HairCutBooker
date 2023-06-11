e56b2de0ba90a1aca985d18d12455de4741455a2
Huyle24 <20521396@hm.uit.edu.vn>
2023-06-11 23:46:17 +0700

    Layout

---
+++ FE-Haircutter/src/routes/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class Home extends Component {
+
+    render() {
+        const { isLoggedIn } = this.props;
+        let linkToRedirect = isLoggedIn ? '/system/user-manage' : '/login';
+        // login roi thi den route user-manageActions, chua thi den login
+
+        return (
+            <Redirect to={linkToRedirect} />
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.admin.isLoggedIn
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
